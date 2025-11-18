583a28584e2c5742300e96b9251d71946868f1c4
Ashraf763 <imashraf07@gmai.com>
2025-11-18 19:34:19 +0530

    added API endpoint

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.post("/", (req, res) => {
+  res.send("Add path '/api/transform' to use the API");
+});
+
+app.post("/api/transform", (req, res) => {
+  const { sentence } = req.body;
+
+  if (!sentence || typeof sentence !== "string") {
+    return res
+      .status(400)
+      .json({ error: "Invalid input, Please provide a valid input" });
+  }
+
+  const words = sentence.split(/\s+/);
+  const uniqueWords = [
+    ...new Set(words.map((word) => word.toLocaleLowerCase())),
+  ];
+  const reversedSentence = words.reverse().join(" ");
+
+  res.json({
+    word_count: words.length,
+    unique_words: uniqueWords,
+    reversed_sentense: reversedSentence,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
